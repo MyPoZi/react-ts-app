88a7680287ca011e2057cf3b9ff1455809a39714
MyPoZi <kxx555u.u@gmail.com>
2020-04-04 17:51:27 +0900

    add: 表示するため

--- src/data.ts
+++ src/data.ts
@@ -3,3 +3,18 @@ export type Row = {
     generation: string
     answers: number[]
 }
+
+export const rows: Rows = [
+    {
+        id: 'up20~un30',
+        generation: '20~30歳',
+        answers: [0.18, 0.22, 0.37, 0.23]
+    },
+    {
+        id: 'up30~un40',
+        generation: '30~40歳',
+        answers: [0.12, 0.20, 0.42, 0.18]
+    }
+]
+
+export type Rows = Row[]
